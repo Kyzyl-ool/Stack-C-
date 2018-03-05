eab06e389ff2b95ac7e93d70cc3237a8722bd39f
Kyzyl-ool <kyzyloolk@mail.ru>
2018-03-05 21:14:22 +0300

    Окончательный вариант

--- stack.cpp
+++ stack.cpp
@@ -1,9 +1,33 @@
 #include <cassert>
 #include <iostream>
-#include "stack.hpp"
-//~ #include "stack_utilities.cpp"
-
 #define STACK_INT_POISON -666666
+
+typedef unsigned long ulong;
+
+typedef enum stack_error_codes
+{
+	STACK_OK = 0,
+	STACK_FULL,
+	STACK_EMPTY,
+	STACK_PUSH_TO_FULL_STACK,
+} stack_error_code;
+
+template <typename T = int, ulong Size = 128>
+class Stack
+{
+private:
+	T data[Size];
+	ulong count;
+
+public:	
+	Stack();
+	~Stack();
+	
+	stack_error_code push(T value);
+	T pop();
+	void dump();
+	stack_error_code check();
+};
 
 template <typename T, ulong Size>
 Stack <T, Size>::Stack():
